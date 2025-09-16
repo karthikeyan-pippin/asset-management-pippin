3c2526f4ba07a414d6eb41a41b428230dd6dccf2
karthikeyan-pippin <karthikeyan@pippintitle.com>
2025-09-16 12:19:19 +0530

    test34

--- .github/workflows/startup.sh
+++ .github/workflows/startup.sh
@@ -39,3 +39,8 @@ export NVM_DIR="$HOME/.nvm"
 echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc
 source ~/.bashrc
+
+sleep 15
+
+cd /var
+mkdir www
